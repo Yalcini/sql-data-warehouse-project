ad1cf4964e86df76cd94a7f57daac6a9faa73d12
Yalcini <97907676+Yalcini@users.noreply.github.com>
2025-05-29 16:59:30 +0300

    Create create_schemas.sql

---
+++ scripts/create_schemas.sql
@@ -0,0 +1,11 @@
+/*
+==========================================================
+Script'in Amacı: 
+    Bu script ile veritabanında bronze, silver ve gold
+    isminde üç schema kurulur.
+==========================================================
+*/
+Create schema bronze;
+Create schema silver;
+Create schema gold;
+
