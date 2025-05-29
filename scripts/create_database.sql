8ed6a9ba0be5289376c60a8400619b581841dd02
Yalcini <97907676+Yalcini@users.noreply.github.com>
2025-05-29 16:55:21 +0300

    Create create_database.sql

---
+++ scripts/create_database.sql
@@ -0,0 +1,13 @@
+/*
+==========================================================
+Script'in Amacı: 
+    Bu script ile "DataWarehouse" isimli yeni bir veritabanı
+    oluşturulur. 
+UYARI:
+    Çalıştırırken başka bir veritabanına bağlı
+    olmanız gerekir. Postgresde 'use' ifadesi olmadığı için
+    veritabanı değişimleri bağlantı seviyesinde yapılır.
+==========================================================
+*/
+
+CREATE DATABASE DataWarehouse;
